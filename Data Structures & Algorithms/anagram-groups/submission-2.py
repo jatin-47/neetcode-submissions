d660da1099052cb1b17ba515e176976817d0d56e
Jatin Saini <67009939+jatin-47@users.noreply.github.com>
2026-07-10 01:15:07 +0530

    Add: anagram-groups - submission-2

---
+++ Data Structures & Algorithms/anagram-groups/submission-2.py
@@ -0,0 +1,28 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        def isAnagram(s, t):
+            if len(s) != len(t):
+                return False
+            alphas = [0]*26
+            for c in s:
+                alphas[ord(c)-ord('a')] = alphas[ord(c)-ord('a')] + 1
+            for c in t:
+                alphas[ord(c)-ord('a')] = alphas[ord(c)-ord('a')] - 1
+
+            for i in alphas:
+                if i != 0:
+                    return False
+            return True
+        anaGrps = []
+        l = len(strs)
+        taken = [0]*l
+        for i in range(l):
+            if taken[i] != 1:
+                anaa = [strs[i]]
+                taken[i] = 1
+                for j in range(i+1, l):
+                    if isAnagram(strs[i], strs[j]):
+                        anaa.append(strs[j])
+                        taken[j] = 1
+                anaGrps.append(anaa)
+        return anaGrps
