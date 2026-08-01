f5ece47be3db520b0b46c4c80dbe1f7e437b86e7
Jatin Saini <67009939+jatin-47@users.noreply.github.com>
2026-08-01 19:23:42 +0530

    Add: longest-consecutive-sequence - submission-0

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-0.py
@@ -0,0 +1,32 @@
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        values = set(nums)
+        checked_seq = {}
+        def get_samllest_item(n):
+            if n-1 in values:
+                get_samllest_item(n-1)
+            return n
+
+        for n in nums:
+            already_done = False
+            for item in checked_seq:
+                if checked_seq[item][0] <= n <= checked_seq[item][1]:
+                    already_done = True
+                    break
+            if already_done:
+                continue
+
+            start = get_samllest_item(n)
+
+            val = start
+            while val in values:
+                val += 1
+
+            checked_seq[val - start] = [start, val-1]
+
+        return max(checked_seq.keys())
+
+
+
+
+        
